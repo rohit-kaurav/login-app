46b9cad8bb1dac892c94b86066c32e780e3bf963
rkaurav <rkaurav@cisco.com>
2018-05-04 18:59:46 +0530

    service change

--- src/app/admin-auth-guard.service.spec.ts
+++ src/app/admin-auth-guard.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { AdminAuthGuardService } from './admin-auth-guard.service';
+import { AdminAuthGuard } from './admin-auth-guard.service';
 
-describe('AdminAuthGuardService', () => {
+describe('AdminAuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AdminAuthGuardService]
+      providers: [AdminAuthGuard]
     });
   });
 
-  it('should ...', inject([AdminAuthGuardService], (service: AdminAuthGuardService) => {
+  it('should ...', inject([AdminAuthGuard], (service: AdminAuthGuard) => {
     expect(service).toBeTruthy();
   }));
 });
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -5,15 +5,10 @@ import { Router, CanActivate } from '@angular/router';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-<<<<<<< HEAD
-constructor(protected router: Router, protected authService: AuthService) { }
- 
-=======
   constructor(
     protected router: Router,
     protected authService: AuthService) { }
 
->>>>>>> df1d91b9832a2755019677c9ee3fcd1092de4700
   canActivate() {
     if (this.authService.isLoggedIn()) return true;
 
